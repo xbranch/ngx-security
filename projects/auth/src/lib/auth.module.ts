d8d14b0949a38066f6ba24a2dff458082affd896
xBrancher <user@xbranch.si>
2018-09-03 23:58:14 +0200

    add: core and auth libs

---
+++ projects/auth/src/lib/auth.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [AuthComponent],
+  exports: [AuthComponent]
+})
+export class AuthModule { }
